d584c7915a413de2c1a3c74f951956b935c42a04
Michael Rog <michael@michaelrog.com>
2010-04-07 15:31:39 -0700

    case error in feed url

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
     $config['name'] = 'RogEE Tweetify';
     $config['version'] = '2.6.0';
-    $config['nsm_addon_updater']['versions_xml'] = 'http://michaelrog.com/go/ee/tweetify/versions.xml';
+    $config['nsm_addon_updater']['versions_xml'] = 'http://michaelrog.com/go/ee/Tweetify/versions.xml';
