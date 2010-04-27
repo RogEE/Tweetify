ac89e1a42845e32af405f742c91ab46cd379e076
Michael Rog <michael@michaelrog.com>
2010-04-26 22:12:54 -0700

    config.php now references versions.xml in repo

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
     $config['name'] = 'RogEE Tweetify';
     $config['version'] = '2.7';
-    $config['nsm_addon_updater']['versions_xml'] = 'http://michaelrog.com/go/ee/Tweetify/versions.xml';
+    $config['nsm_addon_updater']['versions_xml'] = 'http://bitbucket.org/rogee/rogee-tweetify/raw/tip/versions.xml';
