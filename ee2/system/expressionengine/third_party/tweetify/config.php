d31d907daf35b1e7b725f4fa13a5c68580a6dee5
Michael Rog <michael@michaelrog.com>
2013-07-17 10:24:17 -0500

    [stevenlangbroek] Updated hashtag-links to APIv1.1 (https://twitter.com/search instead of http://search.twitter.com)

--- ee2/system/expressionengine/third_party/tweetify/config.php
+++ ee2/system/expressionengine/third_party/tweetify/config.php
@@ -1,4 +1,4 @@
 <?php
     $config['name'] = 'RogEE Tweetify';
-    $config['version'] = '3.2.0';
+    $config['version'] = '3.3.0';
     $config['nsm_addon_updater']['versions_xml'] = 'http://rog.ee/versions/tweetify';
